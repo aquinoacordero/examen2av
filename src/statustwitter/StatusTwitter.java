a5faad50d225983ae2fbd5446cdbf3535bd4e67a
aquinoacordero <aquinoacordero@eniac33.danielcastelao.org>
2015-03-19 09:29:12 +0100

    paso1

--- src/statustwitter/StatusTwitter.java
+++ src/statustwitter/StatusTwitter.java
@@ -18,7 +18,7 @@ public class StatusTwitter {
      * @throws twitter4j.TwitterException
      */
     public static void main(String[] args) throws TwitterException {
-     
+     //paso1
         Twitter mitwitter = new TwitterFactory().getInstance();
  
         Query query = new Query("#Cangas");
